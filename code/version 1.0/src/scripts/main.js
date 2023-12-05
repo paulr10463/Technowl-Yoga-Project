8f48979b0a071cc68301eae054569ec1070e268b
paulr10463 <paul.roman@epn.edu.ec>
2023-12-05 12:37:41 -0500

    feat: Drawer implemented

--- code/version 1.0/src/scripts/main.js
+++ code/version 1.0/src/scripts/main.js
@@ -7,6 +7,26 @@ import YogaPostureController from './controllers/yogaPostureController.js';
 const yogaPostureModel = new YogaPostureService();
 const yogaPostureView = new YogaPosesView();
 const yogaPostureController = new YogaPostureController(yogaPostureModel, yogaPostureView);
+
+
+const drawerButton = document.getElementById('drawerButton');
+drawerButton.addEventListener('click', (event) => {
+    toggleDrawer();
+});
+
+function toggleDrawer() {
+    const drawer = document.querySelector('.drawer');
+    const mainContent = document.querySelector('.main-content');
+
+    if (drawer.style.left === '0px') {
+      drawer.style.left = '-350px';
+      mainContent.style.marginLeft = '0';
+    } else {
+      drawer.style.left = '0';
+      mainContent.style.marginLeft = '350px';
+    }
+  }
+
 //const yogaMorphemesView = new YogaMorphemesView();
 
 //const morphemeModel = new YogaPostureService();
