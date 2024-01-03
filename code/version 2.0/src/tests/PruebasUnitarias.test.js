9cb7367e4e44fa7f2f48e1a0e8e268a3317264b8
paulr10463 <paul.roman@epn.edu.ec>
2024-01-02 20:02:28 -0500

    fix: Tests corrections

--- code/version 2.0/src/tests/PruebasUnitarias.test.js
+++ code/version 2.0/src/tests/PruebasUnitarias.test.js
@@ -4,37 +4,37 @@ import YogaPostureView from '../scripts/views/yogaPostureView.js';
 test('search tadasana name and return postura de la montania', () => {
     const yogaPostureService = new YogaPostureService();
     const tadasanaPosture = yogaPostureService.getPostureByName('tadasana');
-    expect(tadasanaPosture.SpanishName).toBe('Postura de la Montaña');
+    expect(tadasanaPosture.getSpanishName()).toBe('Postura de la Montaña');
   });
 
   test('search Vrikshasana name and return tree pose', () => {
     const yogaPostureService = new YogaPostureService();
     const vrikshasanaPosture = yogaPostureService.getPostureByName('vrikshasana');
-    expect(vrikshasanaPosture.EnglishName).toBe('Tree Pose');
+    expect(vrikshasanaPosture.getEnglishName()).toBe('Tree Pose');
   });
 
   test('search posture name with spaces at the beginning and end', () => {
     const yogaPostureService = new YogaPostureService();
     const vrikshasanaPosture = yogaPostureService.getPostureByName('     vrikshasana    ');
-    expect(vrikshasanaPosture.EnglishName).toBe('Tree Pose');
+    expect(vrikshasanaPosture.getEnglishName()).toBe('Tree Pose');
   });
 
   test('search posture name with spaces at the middle', () => {
     const yogaPostureService = new YogaPostureService();
     const asanaPosture = yogaPostureService.getPostureByName('Ardha         Bhujangasana');
-    expect(asanaPosture.EnglishName).toBe('Half Cobra Pose');
+    expect(asanaPosture.getEnglishName()).toBe('Half Cobra Pose');
   });
 
   test('search posture name with uppercase letters', () => {
     const yogaPostureService = new YogaPostureService();
     const asanaPosture = yogaPostureService.getPostureByName('ARDHA BHUJANGASANA');
-    expect(asanaPosture.EnglishName).toBe('Half Cobra Pose');
+    expect(asanaPosture.getEnglishName()).toBe('Half Cobra Pose');
   });
 
   test('search posture name with uppercase and Lowercase letters', () => {
     const yogaPostureService = new YogaPostureService();
     const asanaPosture = yogaPostureService.getPostureByName('ARdHa BhUJAngAsaNA');
-    expect(asanaPosture.EnglishName).toBe('Half Cobra Pose');
+    expect(asanaPosture.getEnglishName()).toBe('Half Cobra Pose');
   });
 
   test('search posture name with special characters', () => {
